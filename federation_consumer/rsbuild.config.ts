9dd4976371c6f4f7f331450b871401ebb095f9a0
Wanessa <wanessacpr@gmail.com>
2025-02-13 18:31:04 -0300

    feat: primeiro commit

---
+++ federation_consumer/rsbuild.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from '@rsbuild/core';
+import { pluginReact } from '@rsbuild/plugin-react';
+import { pluginModuleFederation } from '@module-federation/rsbuild-plugin';
+
+export default defineConfig({
+  plugins: [
+    pluginReact(),
+    pluginModuleFederation({
+      name: 'federation_consumer',
+      remotes: {
+        federation_provider:
+          'federation_provider@http://localhost:3000/mf-manifest.json',
+        navbar:
+        'navbar@http://localhost:3001/mf-manifest.json',
+      },
+      shared: ['react', 'react-dom'],
+    }),
+  ],
+  server: {
+    port: 2000,
+  },
+});
